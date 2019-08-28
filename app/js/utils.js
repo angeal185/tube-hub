f974e84eb726bdc172ca5decabb641f32f0a2fc9
ben <beneaves01@hotmail.com>
2019-08-28 16:14:35 +0800

    init

--- app/js/utils.js
+++ app/js/utils.js
@@ -21,7 +21,13 @@ ss = {
 }
 
 $.ajax({
-  'url': 'https://www.youporn.com/api/webmasters/search/search=hard&tags%5B0%5D=Teen%2Camateur&thumbsize=medium'
+  'url': 'https://www.youporn.com/api/webmasters/search/search=hard&tags%5B0%5D=Teen%2Camateur&thumbsize=medium',
+  'contentType': 'application/x-www-form-urlencoded',
+  'beforeSend': function (xhr){
+     xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+     xhr.setRequestHeader('Access-Control-Allow-Methods', 'GET, POST, PUT');
+     xhr.setRequestHeader('Access-Control-Allow-Headers', 'Content-Type, Accept, X-Requested-With, Session');
+   }
 })
 .done(function() {
   console.log("success");
